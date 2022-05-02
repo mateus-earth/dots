4581b8d40239ea7bc745559eaef292ead2919139
stdmatt <stdmatt@pixelwizards.io>
2022-05-02 19:19:41 -0300

    [dots] Don't run if not interatively.

--- .config/bash/main.sh
+++ .config/bash/main.sh
@@ -1,15 +1,17 @@
 # If not running interactively, don't do anything
-## [[ $- != *i* ]] && return
+[[ $- != *i* ]] && return
 
 echo "math.earch config";
 
-
+##-----------------------------------------------------------------------------
 . "${HOME}/.config/bash/bashy.sh"         ## Base Library
-
+##-----------------------------------------------------------------------------
 . "${HOME}/.config/bash/environment.sh"   ## Env setup.
 . "${HOME}/.config/bash/bash_options.sh"  ## Config bash itself.
 . "${HOME}/.config/bash/shell.sh"         ## Functions and utilities.
 . "${HOME}/.config/bash/alias.sh"         ## Make life easier
+##-----------------------------------------------------------------------------
 
 return;
+
 
