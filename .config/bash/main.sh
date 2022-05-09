085abf26f52297313ba8793526034454973dac81
mateus-earth <matt@cosmicpig.digital>
2022-05-09 18:12:05 -0300

    [bash] Add ssh.sh to the list of loaded files.

--- .config/bash/main.sh
+++ .config/bash/main.sh
@@ -9,8 +9,11 @@ echo "math.earch config";
 . "${HOME}/.config/bash/environment.sh"   ## Env setup.
 . "${HOME}/.config/bash/bash_options.sh"  ## Config bash itself.
 . "${HOME}/.config/bash/shell.sh"         ## Functions and utilities.
-. "${HOME}/.config/bash/alias.sh"         ## Make life easier
+. "${HOME}/.config/bash/alias.sh"         ## Makes life easier.
+. "${HOME}/.config/bash/ssh.sh"           ## SSH utilities.
 ##-----------------------------------------------------------------------------
+
+test -f "${HOME}/.private/main.sh" && source "${HOME}/.private/main.sh";
 
 return;
 
