0f5a05cc4d003d5a6dba95f24cf6aa0ddf8ab938
mateus-earth <matt@cosmicpig.digital>
2022-05-09 18:51:33 -0300

    [bash] Update history size, editor...

--- .config/bash/environment.sh
+++ .config/bash/environment.sh
@@ -23,13 +23,13 @@ export TERM="xterm-256color" ## getting proper colors
 
 ## History
 export HISTCONTROL=ignoreboth:erasedups
-export HISTSIZE=
-export HISTFILESIZE=
+export HISTSIZE=100000
+export HISTFILESIZE=100000
 export HISTTIMEFORMAT="%Y/%m/%d %H:%M:%S:   "
 
 ## Editor / Pager
-export EDITOR=nvim;
-export VISUAL=nvim;
+export EDITOR=$(which nvim || echo vim);
+export VISUAL=$(which nvim || echo vim);
 export PAGER=less;
 
 ## Locale
@@ -45,6 +45,8 @@ export LC_ALL=en_US.UTF-8
 if [ $bashy_IsGNU ]; then
     PATH_COMPONENTS=(
         ## My stuff...
+        "${HOME}/.bin"
+        "${HOME}/.bin/dots" 
         "${HOME}/.local/bin"
         "${HOME}/.fzf/bin"
         ## Normal stuff...
@@ -57,28 +59,30 @@ if [ $bashy_IsGNU ]; then
         "/usr/bin"
         "/sbin"
         "/bin"
-    );
+    );    
 elif [ $bashy_IsMacOS ]; then
     PATH_COMPONENTS=(
         ## My stuff...
-        "${HOME}/.local/bin",
-        "${HOME}/.fzf/bin",
+        "${HOME}/.bin"
+        "${HOME}/.bin/dots" 
+        "${HOME}/.local/bin"
+        "${HOME}/.fzf/bin"
         "${HOME}/.cargo/bin"
         ## @notice(gnu-tools): Add all the gnu tools to the path
         ## so we can use them in mac without prefixing with g.
         ##    find /usr/local/Cellar -iname "*gnubin" | sort
-        "/usr/local/Cellar/coreutils/9.0_1/libexec/gnubin",
-        "/usr/local/Cellar/ed/1.18/libexec/gnubin",
-        "/usr/local/Cellar/findutils/4.9.0/libexec/gnubin",
-        "/usr/local/Cellar/gawk/5.1.1/libexec/gnubin",
-        "/usr/local/Cellar/gnu-sed/4.8/libexec/gnubin",
-        "/usr/local/Cellar/gnu-tar/1.34/libexec/gnubin",
-        "/usr/local/Cellar/grep/3.7/libexec/gnubin",
-        "/usr/local/Cellar/libtool/2.4.6_4/libexec/gnubin",
-        "/usr/local/Cellar/make/4.3/libexec/gnubin",
+        "/usr/local/Cellar/coreutils/9.0_1/libexec/gnubin"
+        "/usr/local/Cellar/ed/1.18/libexec/gnubin"
+        "/usr/local/Cellar/findutils/4.9.0/libexec/gnubin"
+        "/usr/local/Cellar/gawk/5.1.1/libexec/gnubin"
+        "/usr/local/Cellar/gnu-sed/4.8/libexec/gnubin"
+        "/usr/local/Cellar/gnu-tar/1.34/libexec/gnubin"
+        "/usr/local/Cellar/grep/3.7/libexec/gnubin"
+        "/usr/local/Cellar/libtool/2.4.6_4/libexec/gnubin"
+        "/usr/local/Cellar/make/4.3/libexec/gnubin"
         ## OS Stuff....
-        "/opt/X11/bin",
-        "/usr/local/opt/curl/bin",
+        "/opt/X11/bin"
+        "/usr/local/opt/curl/bin"
         "/usr/local/sbin"
         "/usr/local/bin"
         "/usr/sbin"
