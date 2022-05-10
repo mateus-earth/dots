6851cd844d3cfc63d81c0b2316100df8c8e0ec71
mateus-earth <matt@cosmicpig.digital>
2022-05-10 11:37:18 -0300

    [bash-ssh] Don't create a ssh key if there's already one...

--- .config/bash/ssh.sh
+++ .config/bash/ssh.sh
@@ -1,11 +1,15 @@
 function create-ssh-key()
 {
-    local email="$(git config user.email)";        
-    cat /dev/zero | ssh-keygen -q -N "" -t ed25519 -C "$email";
-    
-    eval "$(ssh-agent -s)";
-    ssh-add ~/.ssh/id_ed25519
+    local email="$(git config user.email)";
+    local ssh_path="$HOME/.ssh/id_ed25519";
+    if [ ! -f "$ssh_path" ]; then 
+        echo "Creating ssh-key at: ($ssh_path)"; ## @todo(echo): Move to log...
 
+        cat /dev/zero | ssh-keygen -q -N "" -t ed25519 -C "$email";
+        eval "$(ssh-agent -s)";
+        ssh-add "$ssh_path";
+    fi;
+    
     echo "---------------------------------------------"
     cat ~/.ssh/id_ed25519.pub;
     echo "---------------------------------------------"
