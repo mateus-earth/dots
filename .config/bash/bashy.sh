a84e708af9dc651491c5ce0f7530ea07304dcf13
mateus-earth <matt@cosmicpig.digital>
2022-05-10 11:36:46 -0300

    [cosmetic]

--- .config/bash/bashy.sh
+++ .config/bash/bashy.sh
@@ -5,12 +5,12 @@ bashy_Platform=$(uname);
 bashy_IsGNU=$(test $bashy_Platform == "Linux" && echo "1");
 bashy_IsMacOS=$(test $bashy_Platform == "Darwin" && echo "1");
 
-
 ## https://stackoverflow.com/a/17841619
-function bashy_join_by {
-  local d=${1-} f=${2-}
-  if shift 2; then
-    printf %s "$f" "${@/#/$d}"
-  fi
+function bashy_join_by()
+{
+    local d=${1-} f=${2-}
+    if shift 2; then
+        printf %s "$f" "${@/#/$d}"
+    fi
 }
 
