8d0b2292a04c871f59d50cc84ec28b83c008398f
mateus-earth <matt@cosmicpig.digital>
2022-05-16 12:40:34 -0300

    [bin] Simplify the clone script.

--- .bin/dots/clone-repos.sh
+++ .bin/dots/clone-repos.sh
@@ -7,48 +7,48 @@
 URL="https://api.github.com/user/repos";
 TMP_FILE="/tmp/matt.repos";
 
-OWN_PATH="$HOME/Projects/public";
-FORK_PATH="$HOME/Projects/forks";
+PUBLIC_PATH="$HOME/Projects/public";
+PRIVATE_PATH="$HOME/Projects/private";
 
 
 ##
-## Funcitons 
+## Functions
 ##
 
 function clone_repo()
 {
     local url="$(echo $1 | sed s/\"//g)";
     local prefix_path="$2";
-    
+
     local name=$(basename "$url" | sed s/\.git//g);
     local target_path="$prefix_path/$name";
-    
+
     test -d $target_path && echo "Already clonned: ($name)" && return;
-    
+
     echo "Clonning repo: ($name) at ($target_path)";
     git clone $url $target_path;
 }
 
-## 
+##
 ## Script
-## 
+##
 
 echo "Getting repo list from github.com...";
 curl                                                                \
      -H "Authorization: token $GITHUB_TOKEN_CLONE_ALL_REPOS" "$URL" \
      -o "$TMP_FILE";
 
-own_repos=$(jq '.[] | select(.owner.login == "mateus-earth") | select(.fork == false) | .ssh_url' $TMP_FILE);
-fork_repos=$(jq '.[] | select(.owner.login == "mateus-earth") | select(.fork == true)  | .ssh_url' $TMP_FILE);
+public_repos=$(jq '.[] | select(.owner.login == "mateus-earth") | select(.private == false) | .ssh_url' $TMP_FILE);
+private_repos=$(jq '.[] | select(.owner.login == "mateus-earth") | select(.private == true)  | .ssh_url' $TMP_FILE);
 
 echo "Clonning personal repos:";
-for url in $own_repos; do 
-    clone_repo $url $OWN_PATH;
+for url in $public_repos; do
+    clone_repo $url $PUBLIC_PATH;
 done;
 
 echo "Clonning forked repos:";
-for url in $fork_repos; do 
-    clone_repo $url $FORK_PATH;
+for url in $private_repos; do
+    clone_repo $url $PRIVATE_PATH;
 done;
 
 echo "Done...";
