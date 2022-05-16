e080e59266947524174b4742df779f5392bae682
mateus-earth <matt@cosmicpig.digital>
2022-05-16 14:29:58 -0300

    [bashy] add the get_arg function.

--- .config/bash/bashy.sh
+++ .config/bash/bashy.sh
@@ -5,6 +5,23 @@ export bashy_Platform=$(uname);
 export bashy_IsGNU=$(test $bashy_Platform == "Linux" && echo "1");
 export bashy_IsMacOS=$(test $bashy_Platform == "Darwin" && echo "1");
 
+
+function bashy_get_arg()
+{
+    local flag="$1"; shift;
+
+    for i in $(seq 1 $#); do
+        if [ "$1" == "$flag" ]; then
+            shift;
+            echo $1;
+            return;
+        fi;
+
+        shift;
+    done;
+}
+
+
 ## https://stackoverflow.com/a/17841619
 function bashy_join_by()
 {
