80d040a47e73a89aecba943e8bcee2fb5b1080fe
mateus-earth <matt@cosmicpig.digital>
2022-05-16 14:30:31 -0300

    [ssh] Refactor the connect-to-server function.

--- .config/bash/ssh.sh
+++ .config/bash/ssh.sh
@@ -1,16 +1,33 @@
 ##-----------------------------------------------------------------------------
-function create-ssh-key()
+test $DOTS_SSH_LOADED && return;
+DOTS_SSH_LOADEDD=1;
+
+##
+## Imports
+##
+
+##-----------------------------------------------------------------------------
+source "$HOME/.config/bash/bashy.sh";
+
+
+##
+## Functions
+##
+
+##-----------------------------------------------------------------------------
+function my-ssh-key()
 {
     local email="$(git config user.email)";
     local ssh_path="$HOME/.ssh/id_ed25519";
-    if [ ! -f "$ssh_path" ]; then 
+
+    if [ ! -f "$ssh_path" ]; then
         echo "Creating ssh-key at: ($ssh_path)"; ## @todo(echo): Move to log...
 
         cat /dev/zero | ssh-keygen -q -N "" -t ed25519 -C "$email";
         eval "$(ssh-agent -s)";
         ssh-add "$ssh_path";
     fi;
-    
+
     echo "---------------------------------------------"
     cat ~/.ssh/id_ed25519.pub;
     echo "---------------------------------------------"
@@ -19,12 +36,16 @@ function create-ssh-key()
 ##-----------------------------------------------------------------------------
 function connect-to-server()
 {
-    local server_name=$(cat ~/.ssh/known_hosts | cut -d" " -f1 | sort | uniq | peco);
+    local server_name="$(bashy_get_arg "--server" $@)";
+    local user_name="$(bashy_get_arg "--user" $@)";
+
+    if [ -z "$server_name" ]; then
+        server_name=$(cat ~/.ssh/known_hosts | cut -d" " -f1 | sort | uniq | peco);
+    fi;
     test -z "$server_name" && echo "Ok..." && return;
 
     local conn_str="$server_name";
-    local user_name="$1";
-    if [ -n "$user_name" ]; then 
+    if [ -n "$user_name" ]; then
         conn_str="$user_name@$server_name";
     fi;
 
