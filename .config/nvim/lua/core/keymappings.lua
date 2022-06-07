7f8f2c057869484aa319c32016b7327e4bfee25d
mateus-earth <matt@cosmicpig.digital>
2022-06-07 16:19:16 -0300

    [nvim] New keymaps for easy align

--- .config/nvim/lua/core/keymappings.lua
+++ .config/nvim/lua/core/keymappings.lua
@@ -24,6 +24,8 @@ keymap("n", "<C-j>", "<C-w>j", opts);
 keymap("n", "<C-k>", "<C-w>k", opts);
 keymap("n", "<C-l>", "<C-w>l", opts);
 
+keymap("n", "ga", "<Plug>(EasyAlign)", opts); -- EasyAlign
+
 -- Resize with arrows
 keymap("n", "<C-Up>",    ":resize -2<CR>",          opts);
 keymap("n", "<C-Down>",  ":resize +2<CR>",          opts);
@@ -55,10 +57,10 @@ keymap("x", "K",     ":move '<-2<CR>gv-gv", opts);
 keymap("x", "<A-j>", ":move '>+1<CR>gv-gv", opts);
 keymap("x", "<A-k>", ":move '<-2<CR>gv-gv", opts);
 
+keymap("x", "ga", "<Plug>(EasyAlign)", opts); -- EasyAlign
 -- Terminal --
 -- Better terminal navigation
 -- keymap("t", "<C-h>", "<C-\\><C-N><C-w>h", term_opts)
 -- keymap("t", "<C-j>", "<C-\\><C-N><C-w>j", term_opts)
 -- keymap("t", "<C-k>", "<C-\\><C-N><C-w>k", term_opts)
 -- keymap("t", "<C-l>", "<C-\\><C-N><C-w>l", term_opts)
-
